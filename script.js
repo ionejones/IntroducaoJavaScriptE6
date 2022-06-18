1e35c62b3b73d12b07a9a0095c169a084b3f15f5
ione <ionejones@gmail.com>
2022-06-17 23:33:57 -0300

    Mostra uso do MAP e do SET

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
 function valoresUnicos(arr) {
   const mySet = new Set(arr);
 
-  return mySet;
+//  return mySet;
+  return [...mySet];
 } 
 
 console.log(valoresUnicos(meuArray));
