65826e429a2b9ead19e652539da6c3580b0a26c4
ione <ionejones@gmail.com>
2022-06-20 21:37:50 -0300

    Exemplos do MAP e SET

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+// Exemplo utilizando MAP sem THIS
+// const nums = [1,2]
 
-function valoresUnicos(arr) {
-  const mySet = new Set(arr);
+// function mapSemThis (arr) {
+//   return arr.map (function(item) {
+//     return item * 2;
+//   })
+// }
 
-//  return mySet;
-  return [...mySet];
-} 
+// console.log ('A mutiplicação com this é ',mapSemThis(nums));
 
-console.log(valoresUnicos(meuArray));
+// Exemplo utilizando MAP com THIS
+// const maca = {
+//   value: 2
+// }
+
+// const laranja = {
+//   value: 3
+// }
+
+// const nums = [1, 2]; 
+
+// function mapComThis (arr, thisArg) {
+//    return  arr.map(function(item) {
+//     return item *this.value;
+//    }, thisArg)
+// }
+
+// console.log('A mutiplicação por maçã é : ',mapComThis(nums, maca));
+
+// console.log('A mutiplicação por laranja é : ',mapComThis(nums, laranja));
+
+// Exemplo utilizando SET
+// const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+// function valoresUnicos(arr) {
+//   const mySet = new Set(arr);
+
+// //  return mySet;
+//   return [...mySet];
+// } 
+
+//console.log(valoresUnicos(meuArray));
 
 // Exemplo utilizando MAP
 // function getAdministradores(map)  {
